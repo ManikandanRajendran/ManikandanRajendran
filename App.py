e603fae7c887b4eadde01fa795a6a56f1cbc8598
ManikandanRajendran <manikandan.rajendran1@centrica.com>
2021-04-06 09:54:18 +0100

    my webside intial design and details

---
+++ App.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import csv
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template("index.html")
+
+
+def write_to_csv(data):
+    with open('database.csv', 'a', newline='') as db1:
+        email = data['email']
+        sub = data['subject']
+        msg = data['message']
+        csv_writer = csv.writer(db1, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, sub, msg])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit():
+    if request.method == "POST":
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return render_template('/thankyou.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
